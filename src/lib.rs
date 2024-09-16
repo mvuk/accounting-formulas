a77740fe125e6a0ca135de89e9985d09e53ce228
Matthew Vuk <matthewvuk@gmail.com>
2024-09-16 18:35:39 -0400

    added contribution margin

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ pub fn break_even_point(fixed_costs: f64, selling_price: f64, variable_costs: f6
     fixed_costs / (selling_price - variable_costs)
 }
 
+pub fn contribution_margin(selling_price: f64, variable_costs: f64) -> f64 {
+    selling_price - variable_costs
+}
 
 #[cfg(test)]
 mod tests {
@@ -15,5 +18,11 @@ mod tests {
     fn test_break_even_point() {
         let result = break_even_point(1000.0, 100.0, 50.0);
         assert_eq!(result, 20.0);
+    }
+
+    #[test]
+    fn test_contribution_margin() {
+        let result = contribution_margin(100.0, 50.0);
+        assert_eq!(result, 50.0);
     }
 }
